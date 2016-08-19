b8734424dc6c1ca49fd08a962e12ebcc280de71f
saikro <saikro@gmail.com>
2016-08-19 18:17:40 +0300

    There were small corrections.

--- impl/excercises/exercise_4.1-to-3_impl.h
+++ impl/excercises/exercise_4.1-to-3_impl.h
@@ -1,3 +1,7 @@
+#include <iterator>
+#include <limits>
+#include <tuple>
+
 namespace vita
 {
   namespace impl
@@ -34,19 +38,113 @@ namespace vita
       return (left, right, max)
     */
 
-	  
+
+    // Excercise 4.1-3 (brute-force implementation)
     template<typename InputIterator>
-    void find_max_subarray_using_brute_force(InputIterator first,
+    auto find_max_subarray_using_brute_force(InputIterator first,
 				             InputIterator last)
     {
+      InputIterator left = last;
+      InputIterator right = last;
+      using ValueType = std::iterator_traits<InputIterator>::value_type;
+      auto max = std::numeric_limits<ValueType>::min();
+      
       for(; first != last; ++first)
       {
-	auto item = *first;
-	for(other = first; other != last; ++other)
+	ValueType sum = 0;
+	for(auto other = first; other != last; ++other)
 	{
-	  
+	  sum += *other;
+	  if(sum > max)
+          {
+	    max = sum;
+	    left = first;
+	    right = other;
+	  }
 	}
       }
+
+      return std::make_tuple(left, right, max);
     }
-  }
-}
+
+    
+    // Excercise 4.1-3(b) (recursive implementation)
+    template<typename InputIterator>
+    auto find_max_crossing_subarray(InputIterator first,
+                                    InputIterator last,
+				    InputIterator middle)
+    {
+      using ValueType = std::iterator_traits<InputIterator>::value_type;
+      auto left_max = std::numeric_limits<ValueType>::min();
+      auto left = last;
+      
+      for(auto left_first = std::make_reverse_iterator(middle),
+          auto left_last  = std::make_reverse_iterator(first),
+	  ValueType sum = 0;
+	  left_first != left_last;
+	  ++left_first)
+      {
+	sum += *left_first;
+	if(sum > left_max)
+	{
+	  left_max = sum;
+	  left = left_first.base();
+	}
+      }
+
+      // right part
+      auto right_max = std::numeric_limits<ValueType>::min();
+      auto right = last;
+      
+      ++middle;
+      for(ValueType sum = 0; middle != last; ++middle)
+      {
+	sum += *middle;
+	if(sum > right_max)
+	{
+	  right_max = sum;
+	  right = middle;
+	}
+      }
+
+      return (left, right, left_max + right_max);
+    }
+        
+    template<typename InputIterator>
+    auto find_max_subarray_recursively(InputIterator first,
+			               InputIterator last)
+    {
+      using ValueType = std::iterator_traits<InputIterator>::value_type;
+      
+      auto size = std::distance(first, last);
+      if(size < 1)
+      {
+	return std::make_tuple(last, last,
+			       std::numeric_limits<ValueType>::min());
+      }
+
+      if(size == 1)
+      {
+	return std::make_tuple(first, last, *first);	
+      }
+
+      auto middle = first;
+      std::advance(first, size/2);
+      
+      auto left_result  = find_max_subarray_recursively(first, middle);
+      auto cross_result = find_max_crossing_subarray(first, last, middle);
+      auto right_result = find_max_subarray_recursively(middle, last);
+
+      constexpr size_t MAX_SUM_IDX = 2;
+      auto left_max  = std::get<MAX_SUM_IDX>(left_result);
+      auto cross_max = std::get<MAX_SUM_IDX>(cross_result);
+      auto right_max = std::get<MAX_SUM_IDX>(rigth_result);
+
+      if(left_max >= cross_max && left_max >= right_max)
+	return left_result;
+      if(cross_max >= left_max && cross_max >= right_max)
+	return cross_result;
+      return right_result;
+    }
+  }  // namespce impl
+}  // namespace vita
